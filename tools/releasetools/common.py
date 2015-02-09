a3f0dbfa94862f0d9e71b2283b2fad1c75cb132d
Readone !! ErcOne <android.armv7a@gmail.com>
2015-02-09 19:47:31 +0700

    build: Add changes to release tools and mkbootimg
    Change the prototype of LoadRecoveryFstab to take in the device type
    as a argument. Fix the case where mkbootimg was being passed a incorrect
    argument.
    
    Change-Id: Ic6ac596d8d96d3a5effbdf513763ec1cb92b1a03

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -142,7 +142,9 @@ def LoadInfoDict(input):
 
   if "fstab_version" not in d:
     d["fstab_version"] = "1"
-
+    
+    if "device_type" not in d:
+    d["device_type"] = "MMC"
   try:
     data = read_helper("META/imagesizes.txt")
     for line in data.split("\n"):
@@ -170,7 +172,7 @@ def LoadInfoDict(input):
   makeint("boot_size")
   makeint("fstab_version")
 
-  d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"])
+  d["fstab"] = LoadRecoveryFSTab(read_helper, d["fstab_version"], d["device_type"])
   d["build.prop"] = LoadBuildProp(read_helper)
   return d
 
@@ -192,7 +194,7 @@ def LoadDictionaryFromLines(lines):
       d[name] = value
   return d
 
-def LoadRecoveryFSTab(read_helper, fstab_version):
+def LoadRecoveryFSTab(read_helper, fstab_version, type):
   class Partition(object):
     pass
 
@@ -343,6 +345,11 @@ def BuildBootableImage(sourcedir, fs_config_file, info_dict=None):
       cmd.append("--tags-addr")
       cmd.append(open(fn).read().rstrip("\n"))
 
+    fn = os.path.join(sourcedir, "tags_offset")
+    if os.access(fn, os.F_OK):
+      cmd.append("--tags_offset")
+      cmd.append(open(fn).read().rstrip("\n"))
+      
     fn = os.path.join(sourcedir, "ramdisk_offset")
     if os.access(fn, os.F_OK):
       cmd.append("--ramdisk_offset")
